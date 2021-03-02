d437af07a5cda6d1ef76bbf5a9f014bc85433b8c
SimonLeclere <simon-leclere@orange.fr>
2021-03-02 21:47:49 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Quiz struct {
+	Id           int       `json:"id"`
+	Question     string    `json:"question"`
+	Propositions [4]string `json:"propositions"`
+	Réponse      string    `json:"réponse"`
+	Anecdote     string    `json:"anecdote"`
+}
+
+var quizzs []Quiz
+
+func getRandomQuiz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	question := quizzs[rand.Intn(len(quizzs))]
+
+	json.NewEncoder(w).Encode(question)
+}
+
+func getQuizByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range quizzs {
+		toCompare, _ := strconv.Atoi(params["id"])
+		if item.Id == toCompare {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
+}
+
+func readFile(path string) {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &quizzs)
+}
+
+func main() {
+
+	readFile("quizzs.json")
+
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/quiz/", getRandomQuiz).Methods("GET")
+	r.HandleFunc("/quiz/{id}", getQuizByID).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
